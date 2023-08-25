beb767fb79e94ece1869c7863b87733af72655fe
Johannes Tuikkala <johannes@vaadin.com>
2023-08-25 11:21:00 +0300

    solution

--- exercise-numeric-textfield/src/main/java/com/vaadin/training/clientside/numerictextfield/NumericTextField.java
+++ exercise-numeric-textfield/src/main/java/com/vaadin/training/clientside/numerictextfield/NumericTextField.java
@@ -1,7 +1,29 @@
 package com.vaadin.training.clientside.numerictextfield;
 
+import com.vaadin.data.Validator.InvalidValueException;
+import com.vaadin.data.util.converter.Converter.ConversionException;
+
 public class NumericTextField extends com.vaadin.ui.TextField {
 
     public NumericTextField() {
     }
+
+    @Override
+    protected void setValue(String newFieldValue, boolean repaintIsNotNeeded)
+            throws ReadOnlyException,
+            ConversionException, InvalidValueException {
+        if (newFieldValue == null || "".equals(newFieldValue)) {
+            super.setValue(null, repaintIsNotNeeded);
+            return;
+        }
+        try {
+            // Just parse it to see, if an exception occurs
+            Integer.parseInt(newFieldValue);
+            super.setValue(newFieldValue, repaintIsNotNeeded);
+        } catch (NumberFormatException e) {
+            throw new InvalidValueException(
+                    "The value " + newFieldValue + " was not an integer");
+        }
+    }
+
 }
--- exercise-numeric-textfield/src/main/java/com/vaadin/training/clientside/numerictextfield/client/NumericTextFieldConnector.java
+++ exercise-numeric-textfield/src/main/java/com/vaadin/training/clientside/numerictextfield/client/NumericTextFieldConnector.java
@@ -1,16 +1,50 @@
 package com.vaadin.training.clientside.numerictextfield.client;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
 import com.vaadin.client.ui.VTextField;
 import com.vaadin.client.ui.textfield.TextFieldConnector;
 import com.vaadin.shared.ui.Connect;
 import com.vaadin.training.clientside.numerictextfield.NumericTextField;
 
+@SuppressWarnings("serial")
 @Connect(NumericTextField.class)
 public class NumericTextFieldConnector extends TextFieldConnector {
 
+    private List<Integer> acceptedKeyCodes = new ArrayList<Integer>();
+
     public NumericTextFieldConnector() {
-        // TODO: add keydown handler to the widget that allows only numbers
+        java.util.logging.Logger.getLogger(getClass().getSimpleName())
+                .log(Level.WARNING, "Hey there");
+        acceptedKeyCodes.add(KeyCodes.KEY_ZERO);
+        acceptedKeyCodes.add(KeyCodes.KEY_ONE);
+        acceptedKeyCodes.add(KeyCodes.KEY_TWO);
+        acceptedKeyCodes.add(KeyCodes.KEY_THREE);
+        acceptedKeyCodes.add(KeyCodes.KEY_FOUR);
+        acceptedKeyCodes.add(KeyCodes.KEY_FIVE);
+        acceptedKeyCodes.add(KeyCodes.KEY_SIX);
+        acceptedKeyCodes.add(KeyCodes.KEY_SEVEN);
+        acceptedKeyCodes.add(KeyCodes.KEY_EIGHT);
+        acceptedKeyCodes.add(KeyCodes.KEY_NINE);
+        acceptedKeyCodes.add(KeyCodes.KEY_BACKSPACE);
+        acceptedKeyCodes.add(KeyCodes.KEY_DELETE);
+        acceptedKeyCodes.add(KeyCodes.KEY_LEFT);
+        acceptedKeyCodes.add(KeyCodes.KEY_RIGHT);
+
+        getWidget().addKeyDownHandler(new KeyDownHandler() {
+            @Override
+            public void onKeyDown(KeyDownEvent event) {
+                if (!acceptedKeyCodes.contains(event.getNativeKeyCode())) {
+                    event.preventDefault();
+                }
+            }
+        });
     }
 
     @Override
